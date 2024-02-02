7446c30364e586f6df3f50c3d7974e00383fc4a6
Birkin James Diana <birkin.diana@gmail.com>
2024-02-02 07:49:19 -0500

    adds "v" to commit-hash.

--- build.rs
+++ build.rs
@@ -12,12 +12,13 @@ fn main() {
         .expect("Failed to execute git command");
 
     let git_hash = String::from_utf8_lossy(&output.stdout).trim().to_string();
+    let version_string: String = format!("v{}", git_hash);
 
     // Write the git hash to a file that can be included in the main binary
-    let out_dir = env::var("OUT_DIR").unwrap();
+    let out_dir: String = env::var("OUT_DIR").unwrap();
     let dest_path = Path::new(&out_dir).join("git_commit.rs");
     let mut f = File::create(&dest_path).unwrap();
 
     // Write the git commit hash to the file
-    write!(f, "pub const GIT_COMMIT: &str = \"{}\";", git_hash).unwrap();
+    writeln!(f, "pub const GIT_COMMIT: &str = \"{}\";", version_string).unwrap();
 }
